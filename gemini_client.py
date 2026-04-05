83a8862e3f1783a8c48964363b13409fa8fa7498
Renat Gaibatov <gaibatovrenat@yandex.ru>
2026-04-05 16:34:48 +0300

    gemini_client.py: добавлена функция get_answer_from_gemini для обращения с промтом к gemini

--- gemini_client.py
+++ gemini_client.py
@@ -11,7 +11,11 @@ config_obj = Config()
 
 client = genai.Client(api_key=config_obj.gemini_api_key)
 
-response = client.models.generate_content(
-    model="gemini-2.5-flash", contents="Explain how AI works in a few words"
-)
-print(response.text)
+
+def get_answer_from_gemini(prompt: str) -> str:
+    response = client.models.generate_content(
+        model="gemini-2.5-flash-lite",
+        contents=prompt
+    )
+
+    return response.text
