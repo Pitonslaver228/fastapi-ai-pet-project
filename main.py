a625cdf8c50c3910f2b12e186566f82d2c334f68
Renat Gaibatov <gaibatovrenat@yandex.ru>
2026-04-05 16:47:29 +0300

    main: добавлена ручка для post-запросов, промт отправляется не в заголовке, а в теле Body

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
+from gemini_client import get_answer_from_gemini
 
 
 app = FastAPI()
@@ -7,3 +8,11 @@ app = FastAPI()
 @app.get("/requests")
 def get_my_requests():
     return "Hello, World!"
+
+@app.post("/requests")
+def send_prompt(
+        prompt = Body(embed=True)
+):
+    answer = get_answer_from_gemini(prompt)
+
+    return {"answer": answer}
